4f13286977b079790b45988145a4e65922de19ff
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-06-19 11:32:43 +0700

    Initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {Sidebar} from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <Sidebar/>
+    <main
+        :class="[
+          'min-h-screen px-4 pt-16 transition-all duration-200 md:px-6 md:pt-6 md:pl-60',
+        ]"
+    >
+      <div class="space-y-6 ml-4">
+        <slot/>
+      </div>
+    </main>
+  </div>
+</template>

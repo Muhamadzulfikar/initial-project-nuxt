fbd1addcb1643d0a17f2ce02bd5a4a758531cf12
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-06-25 15:09:07 +0700

    feat: add header and breadcrumb in page datatable

--- app/pages/datatable.vue
+++ app/pages/datatable.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import type { SelectItem } from '@nuxt/ui'
+import type {SelectItem, BreadCrumbItem} from '@nuxt/ui'
+
 const loading = ref(false)
 const dataTableRef = ref()
-const statusFilter = ref('')
+const statusFilter = ref()
 
-const statusOptions = <SelectItem[]> ([
-  {value: '', label: 'All Status'},
+const statusOptions = <SelectItem[]>([
+  {value: null, label: 'All Status'},
   {value: 'paid', label: 'Paid'},
   {value: 'failed', label: 'Failed'},
   {value: 'refunded', label: 'Refunded'}
 ]);
+
+const breadcrumbs = <BreadCrumbItem[]>([
+  {label: 'Datatable', icon: 'i-lucide-table',  to: '/datatable'}
+])
 
 onMounted(() => {
   dataTableRef.value?.fetchData()
@@ -17,34 +22,37 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <UButton size="md" icon="i-lucide-plus" class="mb-4">Add Data</UButton>
-
-    <div class="mb-4">
-      <USelect
-          v-model="statusFilter"
-          :items="statusOptions"
-          placeholder="Filter by status"
-      />
-    </div>
-
-    <UDataTable
-        ref="dataTableRef"
-        server-mode
-        api-url="/api/datatables"
-        :loading=true
-        :filters="{ status: statusFilter }"
-        @update:loading="loading = $event"
-    >
-      <template #status-cell="{ row }">
-        <UBadge color="primary">
-          {{ row.original.status }}
-        </UBadge>
-      </template>
-
-      <template #actions-cell="{ row }">
-        <UButton size="xs" color="red" variant="soft">Delete {{ row.original.id }}</UButton>
-      </template>
-    </UDataTable>
+  <div class="flex justify-between items-center mb-4">
+    <h1 class="font-bold text-2xl">Data Table</h1>
+     <UBreadcrumb class="text-x" :items="breadcrumbs" />
   </div>
+  <UButton size="md" icon="i-lucide-plus" class="mb-4">Add Data</UButton>
+
+  <UCard class="mb-4 flex">
+    <USelect
+        v-model="statusFilter"
+        :items="statusOptions"
+        placeholder="Filter by status"
+        class="mr-4"
+    />
+  </UCard>
+
+  <UDataTable
+      ref="dataTableRef"
+      server-mode
+      api-url="/api/datatables"
+      :loading=true
+      :filters="{ status: statusFilter }"
+      @update:loading="loading = $event"
+  >
+    <template #status-cell="{ row }">
+      <UBadge color="primary">
+        {{ row.original.status }}
+      </UBadge>
+    </template>
+
+    <template #actions-cell="{ row }">
+      <UButton size="xs" color="red" variant="soft">Delete {{ row.original.id }}</UButton>
+    </template>
+  </UDataTable>
 </template>

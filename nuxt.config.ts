b4f9a52b71e140421a236c8c18ffb6d325eec0eb
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-06-26 15:28:05 +0700

    feat: delete unused code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
-import winston from 'winston'
-import DailyRotateFile from 'winston-daily-rotate-file'
-import path from 'node:path'
-
-const createDailyTransport = (filename: string, level: string) => {
-    return new DailyRotateFile({
-        filename: path.resolve(process.cwd(), `logs/${filename}-%DATE%.log`),
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '30d',
-        level: level
-    })
-}
-
-const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
-    transports: [
-        createDailyTransport('activity', 'info'),
-        createDailyTransport('error', 'error')
-    ]
-})
-
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
@@ -55,9 +28,5 @@ export default defineNuxtConfig({
         env: {
             service: 'initial-project-nuxt',
         },
-        drain: async (event) => {
-            const isError = event.error || (event.data && event.data.error)
-            isError ? logger.error(event) : logger.info(event)
-        }
     },
 })

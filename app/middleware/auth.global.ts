f7b0ae1ec0446ffacd169d44b33a1b8326e0cbc8
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-06-26 13:53:47 +0700

    fix: bug login

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,8 +1,17 @@
-import { useLogtoUser } from '#imports';
-export default defineNuxtRouteMiddleware((to, from) => {
-  const user = useLogtoUser();
+import {useLogtoUser} from '#imports';
 
-  if (!user) {
-    return navigateTo('/login')
-  }
+export default defineNuxtRouteMiddleware((to, from) => {
+    const user = useLogtoUser();
+
+    const publicRoutes = ['/login', '/auth/callback', '/logout']
+
+    if (!user && !publicRoutes.includes(to.path)) {
+        return navigateTo('/login')
+    }
+
+    if (user && to.path === '/login') {
+
+        return navigateTo('/')
+
+    }
 })

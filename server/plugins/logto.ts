e8390a8c635cefbb52a5bec2e07b108701fbae5b
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2026-06-26 13:58:54 +0700

    feat: delete unused code

--- server/plugins/logto.ts
+++ server/plugins/logto.ts
@@ -1,9 +1,5 @@
 import { useRuntimeConfig } from '#imports'
 
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
-
-  if (config.logto?.endpoint && config.logto?.appId) {
-    await logtoEventHandler(event, config)
-  }
+  useRuntimeConfig()
 })
